2b16aa6aea122c6fa90b91f66b612cd193b5a385
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-26 15:54:48 +0700

    Puppeteer can follow url change, mainFrame is all about main url page

--- src/findStore.js
+++ src/findStore.js
@@ -2,14 +2,22 @@ const puppeteer = require('puppeteer');
 const {puppeteer : config} = require("./config")
 const defer = require("./defer")
 
+const dumpFrameTree = (frame, indent) => {
+  console.log(indent + frame.url());
+  for (let child of frame.childFrames())
+    dumpFrameTree(child, indent + '  ');
+}
+
 const findStore = async (homepage) => {
+
   const browser = await puppeteer.launch(config.launch);
   const page = await browser.newPage();
   await page.goto(homepage);
   await defer(3)
   const popup = await page.$("#popup-choose-category > ul > li:nth-child(1) > a");
   await popup.click();
-  await defer(1)
+  await defer(1);
+  await page.screenshot({path: `before.png`})
 
   const steps = [
     "#searchFormTop > div > a",
@@ -19,14 +27,58 @@ const findStore = async (homepage) => {
     "#fdDlgSearchFilter > div.sf-bottom > div > a.fd-btn.blue",
   ]
 
-  await steps.map(async selector => {
+  // const s0 = await page.$(steps[0]);
+  // if(s0){
+  //   await s0.click();
+  //   await page.screenshot({path: `s0.png`})
+  // }
+
+
+
+  // const s1 = await page.$(steps[1]);
+  // if(s1){
+  //
+  // }
+  // await s1.click();
+  //
+  // const s2 = await page.$(steps[2]);
+  // await s2.click();
+  //
+  // await page.screenshot({path: `s2.png`})
+  //
+  // const s3 = await page.$(steps[3]);
+  // await s3.click();
+  //
+  // const s4 = await page.$(steps[4]);
+  // await s4.click();
+
+  // await defer(4);
+
+  // await page.screenshot({path: `s4.png`})
+
+  await steps.map(async (selector, index) => {
     const e = await page.$(selector);
-     if(e){
+    if(e){
        console.log("See e")
        await e.click();
        await defer(1)
+       await page.screenshot({path: `s${index}.png`})
+     }else{
+       console.log("Not see e")
      }
   })
+
+  // await defer(5);
+  await page.evaluate(() => {
+    window.location.href = "https://www.google.com.vn/?gfe_rd=cr&dcr=0&ei=8hTKWbOkBYXM8gf_zoagDA"
+    return;
+  });
+
+  await defer(2)
+
+  await page.screenshot({path: `after.png`})
+
+  // dumpFrameTree(page.mainFrame(), '');
 
   // const pageInfo = await page.evaluate(async () => {
   //   const defer = async waitTime => await new Promise(resolve => setTimeout(resolve, waitTime * 1000));
@@ -86,7 +138,6 @@ const findStore = async (homepage) => {
   //   return currUrl;
   // });
 
-  await page.screenshot({path: 'example.png'})
   // Clear browser before out
   await browser.close()
   // Return
