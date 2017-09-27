837b208eb9a17215ce1f51c2c68d56b09cb7a04d
root <root@vagrant.vm>
2017-09-27 02:39:53 +0000

    puppeteer fail both click/touch, damn it

--- src/findStore.js
+++ src/findStore.js
@@ -11,22 +11,27 @@ const findStore = async homepage => {
   const browser = await puppeteer.launch(config.launch)
   const page = await browser.newPage()
   await page.goto(homepage)
+  await page.setViewport({
+    width: 1200,
+    height: 1014
+  })
   await defer(3)
   const popup = await page.$("#popup-choose-category > ul > li:nth-child(1) > a")
   await popup.click()
   await defer(1)
   await page.screenshot({ path: `before.png` })
 
-  const filterE = await page.$("#searchFormTop > div > a")
-  await filterE.click()
-
-  // await page.waitForSelector("#fdDlgSearchFilter > div.sf-right")
-  await defer(10)
-  console.log("After click do you see filterContent")
-
-  const s1 = await page.$("#search-filter-dis-4")
-  console.log("See s1 or not", Boolean(s1))
-  await s1.click()
+  // Try to click on select box
+  const bound = await page.evaluate(() => {
+    const selectBox = document.querySelector("#tbt > ul > li:nth-child(7) > select")
+    const DOMbound = selectBox.getBoundingClientRect()
+    const { x, y, width, height } = DOMbound
+    return { x, y, width, height }
+  })
+  console.log(bound)
+  // await page.mouse.click(bound.x + 5, bound.y + 5)
+  await page.touchscreen.tap(bound.x + bound.width / 2, bound.y + bound.height / 2)
+  await defer(3)
 
   await page.screenshot({ path: `after.png` })
   await browser.close()
