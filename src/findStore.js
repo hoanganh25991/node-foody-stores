8477dc7a06c748b912520c8ede7bbc35e2f35b32
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-27 05:07:14 +0000

    Refactor but not success, pure function A => B => C not works

--- src/findStore.js
+++ src/findStore.js
@@ -7,22 +7,55 @@ const dumpFrameTree = (frame, indent) => {
   for (let child of frame.childFrames()) dumpFrameTree(child, indent + "  ")
 }
 
+const dismisPopup = async page => async selector => {
+  const popup = await page.$(selector)
+  await popup.click()
+  /**
+   * We check offsetParent as NULL to check if element visible or not
+   * @see https://stackoverflow.com/questions/19669786/check-if-element-is-visible-in-dom#21696585
+   */
+  await page.waitForFunction(`document.querySelector(${selector}).offsetParent === null`)
+}
+
+const timeout = {
+  timeoutCount: 0, // count as seconds
+  store: (timeout, type = "s") => {
+    let added, puppeteerTimeout
+    switch (type) {
+      case "s":
+        added = timeout
+        puppeteerTimeout = timeout * 1000
+        break
+      case "ms":
+        added = timeout / 1000
+        puppeteerTimeout = timeout
+        break
+      default:
+        throw new Error("Unknown time format")
+    }
+    this.timeoutCount += added
+    return { timeout: puppeteerTimeout }
+  },
+  log: () => {
+    console.log(`[INFO] You have timeout for ${this.timeoutCount}`)
+  }
+}
+
+timeout.store(5)
+
+const clickAndWait = async page => async ({ selector, waitFor = "body" }, index) => {
+  const stepButton = await page.$(selector)
+  await stepButton.click()
+  await page.waitForSelector(waitFor, timeout.store(5))
+  await page.screenshot({ path: `step${index}.png` })
+}
+
 const findStore = async homepage => {
   const browser = await puppeteer.launch(config.launch)
   const page = await browser.newPage()
   await page.goto(homepage)
-  await page.setViewport({
-    width: 1200,
-    height: 1014
-  })
-  // page.on('dialog', async dialog => {
-  //   console.log(dialog.message());
-  //   await dialog.dismiss();
-  // });
-  const popup = await page.$("#popup-choose-category > ul > li:nth-child(1) > a")
-  await popup.click()
-  await defer(1)
-
+  await page.setViewport({ width: 1200, height: 1014 })
+  await dismisPopup(page)("#popup-choose-category > ul > li:nth-child(1) > a")
   await page.screenshot({ path: `before.png` })
 
   const steps = [
@@ -33,28 +66,11 @@ const findStore = async homepage => {
     { selector: "#fdDlgSearchFilter > div.sf-bottom > div > a.fd-btn.blue", waitFor: "" }
   ]
 
-  // await steps.map(async ({selector, waitFor}, index) => {
-  //   const stepButton = await page.$(selector);
-  //   await stepButton.click();
-  //   await page.waitForSelector(waitFor, {timeout: 5*1000})
-  //   await page.screenshot({path: `step${index}.png`})
-  // })
+  await steps.map(async ({ selector, waitFor }, index) => {
+    await clickAndWait(page)({ selector, waitFor }, index)
+  })
 
-  const filterE = await page.$("#searchFormTop > div > a")
-  await filterE.click()
-  await page.waitForSelector("#fdDlgSearchFilter > div.sf-right")
-  console.log("After click do you see filterContent")
-  const s1 = await page.$("#search-filter-dis-4")
-  console.log("See s1 or not", Boolean(s1))
-  await s1.click()
-
-  // let  {selector, waitFor = "body"} = steps[0]
-  // const stepButton = await page.$(selector);
-  // await stepButton.click();
-  // console.log(waitFor)
-  // // await page.waitForSelector(waitFor, {timeout: 10*1000})
-  // await page.waitForSelector(waitFor)
-  // await page.screenshot({path: `step${1}.png`})
+  timeout.log()
 
   await defer(3)
   await page.screenshot({ path: `after.png` })
