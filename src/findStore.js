868c8a3ecd259057f0a0c454f77b90534996f779
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-27 03:32:14 +0000

    REMOVE POPUP is damn important, any futher click not work, if popup not dismiss

--- src/findStore.js
+++ src/findStore.js
@@ -15,29 +15,48 @@ const findStore = async homepage => {
     width: 1200,
     height: 1014
   })
-  page.on("console", (...args) => {
-    for (let i = 0; i < args.length; ++i) console.log(`${i}: ${args[i]}`)
-  })
-
-  await defer(1)
-  // Click to dimiss popup
+  // page.on('dialog', async dialog => {
+  //   console.log(dialog.message());
+  //   await dialog.dismiss();
+  // });
   const popup = await page.$("#popup-choose-category > ul > li:nth-child(1) > a")
   await popup.click()
   await defer(1)
+
   await page.screenshot({ path: `before.png` })
 
-  await page.evaluate(() => {
-    document.addEventListener("click", () => console.log("adf"))
-    console.log("hello")
-    return
-  })
+  const steps = [
+    { selector: "#searchFormTop > div > a", waitFor: "#fdDlgSearchFilter > div.sf-right" },
+    { selector: "#search-filter-dis-4" },
+    { selector: "#fdDlgSearchFilter > div.sf-left > ul > li:nth-child(3)", waitFor: "#search-filter-cate-11" },
+    { selector: "#search-filter-cate-11" },
+    { selector: "#fdDlgSearchFilter > div.sf-bottom > div > a.fd-btn.blue", waitFor: "" }
+  ]
 
-  // await page.mouse.click(790, 620); // Try to click on selectbox
-  await page.mouse.click(265, 600) // Try to click on "Gần tôi"
-  // const e = await page.$("#tbt > ul > li:nth-child(2) > a") // Try to click on "Gần tôi"
-  // await e.click();
+  // await steps.map(async ({selector, waitFor}, index) => {
+  //   const stepButton = await page.$(selector);
+  //   await stepButton.click();
+  //   await page.waitForSelector(waitFor, {timeout: 5*1000})
+  //   await page.screenshot({path: `step${index}.png`})
+  // })
 
-  await defer(6)
+  const filterE = await page.$("#searchFormTop > div > a")
+  await filterE.click()
+  await page.waitForSelector("#fdDlgSearchFilter > div.sf-right")
+  console.log("After click do you see filterContent")
+  const s1 = await page.$("#search-filter-dis-4")
+  console.log("See s1 or not", Boolean(s1))
+  await s1.click()
+
+  // let  {selector, waitFor = "body"} = steps[0]
+  // const stepButton = await page.$(selector);
+  // await stepButton.click();
+  // console.log(waitFor)
+  // // await page.waitForSelector(waitFor, {timeout: 10*1000})
+  // await page.waitForSelector(waitFor)
+  // await page.screenshot({path: `step${1}.png`})
+
+  await defer(3)
   await page.screenshot({ path: `after.png` })
   await browser.close()
   return "hello"
