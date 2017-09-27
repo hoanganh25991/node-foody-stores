c969fbcdb14e53843186c3aebccb753a0cd289e1
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-27 07:36:48 +0000

    OH YEAHH, complete pick filter, jump to result page

--- src/findStore.js
+++ src/findStore.js
@@ -1,8 +1,10 @@
 const puppeteer = require("puppeteer")
 const { puppeteer: config } = require("./config")
 const defer = require("./defer")
+const logWithInfo = require("./logWithInfo")
+const logExactErrMsg = require("./logExactErrMsg")
 
-const timeout = {
+const TM = {
   timeoutCount: 0, // count as seconds
   store: (timeout, type = "s") => {
     let added, puppeteerTimeout
@@ -18,12 +20,16 @@ const timeout = {
       default:
         throw new Error("Unknown time format")
     }
-    this.timeoutCount += added
+    TM.timeoutCount += added
     return { timeout: puppeteerTimeout }
   },
   log: () => {
-    console.log(`[INFO] You have timeout for ${this.timeoutCount}`)
+    logWithInfo(`You have timeout for ${TM.timeoutCount}s`)
   }
+}
+
+const screenshot = page => async options => {
+  await page.screenshot(options)
 }
 
 const dismisPopup = page => async selector => {
@@ -34,32 +40,32 @@ const dismisPopup = page => async selector => {
    * @see https://stackoverflow.com/questions/19669786/check-if-element-is-visible-in-dom#21696585
    */
   const cmd = `document.querySelector("${selector}").offsetParent === null`
-  await page.waitForFunction(cmd)
+  await page.waitForFunction(cmd, TM.store(5))
 }
 
 const clickAndWait = page => async ({ selector, waitFor = "body" }, index) => {
   const stepButton = await page.$(selector)
+  logWithInfo(`Find ${selector}: ${Boolean(stepButton)}`)
   await stepButton.click()
-  await page.waitForSelector(waitFor)
-  await page.screenshot({ path: `step${index}.png` })
+  await page.waitForSelector(waitFor, TM.store(5))
+  await screenshot(page)({ path: `step${index}.jpeg`, quality: 100 })
 }
 
 const findStore = async homepage => {
   const browser = await puppeteer.launch(config.launch)
   const page = await browser.newPage()
   await page.goto(homepage)
-  await page.setViewport({ width: 1200, height: 1014 })
+  // await page.setViewport({ width: 1200, height: 1014 })
   await dismisPopup(page)("#popup-choose-category > ul > li:nth-child(1) > a")
-  await page.screenshot({ path: `before.png` })
+  await screenshot(page)({ path: `before.png` })
 
   const steps = [
     { selector: "#searchFormTop > div > a", waitFor: "#fdDlgSearchFilter > div.sf-right" },
     { selector: "#search-filter-dis-4" },
     { selector: "#fdDlgSearchFilter > div.sf-left > ul > li:nth-child(3)", waitFor: "#search-filter-cate-11" },
     { selector: "#search-filter-cate-11" },
-    { selector: "#fdDlgSearchFilter > div.sf-bottom > div > a.fd-btn.blue", waitFor: "" }
+    { selector: "#fdDlgSearchFilter > div.sf-bottom > div > a.fd-btn.blue" }
   ]
-
   /**
    * @notice
    * Please remeber that map/forEach CANT run async/await in concept of
@@ -75,15 +81,16 @@ const findStore = async homepage => {
   //   await clickAndWait(page)({ selector, waitFor }, index)
   // })
 
-  steps.reduce(async (carry, { selector, waitFor }, index) => {
+  const startQueue = Promise.resolve(logWithInfo("Starting await queue..."))
+  await steps.reduce(async (carry, { selector, waitFor }, index) => {
     await carry
     return clickAndWait(page)({ selector, waitFor }, index)
-  }, Promise.resolve(console.log("Starting await queue...")))
-
-  timeout.log()
-
-  await defer(3)
-  await page.screenshot({ path: `after.png` })
+  }, startQueue)
+  TM.log()
+  await page.waitForSelector(
+    "#GalleryPopupApp > div.directory-container > div > div > div > div > div.result-side > div.head-result.d_resultfilter"
+  )
+  await screenshot(page)({ path: `after.png` })
   await browser.close()
   return "hello"
 }
