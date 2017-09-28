77a3e32e3915911811a5ac94251f651d65f7841d
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-28 03:09:01 +0000

    [API] another example with EXPOST, damn good when it can bring logic between two

--- tryApi.js
+++ tryApi.js
@@ -26,24 +26,46 @@ const launchConfig = {
 //   })
 
 /**
- *
+ * Expost outside world function INTO evaluate context
  */
-const crypto = require("crypto")
+// const crypto = require("crypto")
+//
+// puppeteer.launch(launchConfig).then(async browser => {
+//   const page = await browser.newPage()
+//   page.on("console", console.log)
+//   await page.exposeFunction("md5", text =>
+//     crypto
+//       .createHash("md5")
+//       .update(text)
+//       .digest("hex")
+//   )
+//   await page.evaluate(async () => {
+//     // use window.md5 to compute hashes
+//     const myString = "PUPPETEER"
+//     const myHash = await window.md5(myString)
+//     console.log(`md5 of ${myString} is ${myHash}`)
+//   })
+//   await browser.close()
+// })
+
+// Another example with fs
+const fs = require("fs")
 
 puppeteer.launch(launchConfig).then(async browser => {
   const page = await browser.newPage()
   page.on("console", console.log)
-  await page.exposeFunction("md5", text =>
-    crypto
-      .createHash("md5")
-      .update(text)
-      .digest("hex")
-  )
+  await page.exposeFunction("readfile", async filePath => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, "utf8", (err, text) => {
+        if (err) reject(err)
+        else resolve(text)
+      })
+    })
+  })
   await page.evaluate(async () => {
-    // use window.md5 to compute hashes
-    const myString = "PUPPETEER"
-    const myHash = await window.md5(myString)
-    console.log(`md5 of ${myString} is ${myHash}`)
+    // use window.readfile to read contents of a file
+    const content = await window.readfile("./package.json")
+    console.log(content)
   })
   await browser.close()
 })
