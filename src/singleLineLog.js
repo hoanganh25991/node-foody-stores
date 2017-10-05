90cf0fdadad06df8b2a5a39608ba7e773ae6d984
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-05 07:29:22 +0000

    Single line as show loading percent

---
+++ src/singleLineLog.js
@@ -0,0 +1,20 @@
+var log = require("single-line-log").stdout
+const fs = require("fs")
+// or pass any stream:
+// var log = require('single-line-log')(process.stdout);
+const filePath = `${__dirname}/../tmp/crawling-foody-stores_2017-10-04_232536.wmv`
+var read = 0
+var size = fs.statSync(filePath).size
+var rs = fs.createReadStream(filePath)
+rs.on("data", function(data) {
+  read += data.length
+  var percentage = Math.floor(100 * read / size)
+
+  // Keep writing to the same two lines in the console
+  log("Writing to super large file\n[" + percentage + "%]", read, "bytes read")
+})
+
+rs.on("end", () => {
+  console.log("")
+  console.log("asdfasdF")
+})
