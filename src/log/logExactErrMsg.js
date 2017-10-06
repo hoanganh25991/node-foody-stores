e9eca312425d8ec4a73bb720fae05857ab158526
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-06 09:02:04 +0000

    Refactor logErr

--- src/log/logExactErrMsg.js
+++ src/log/logExactErrMsg.js
@@ -1,5 +1,3 @@
-const logWithInfo = require("./logWithInfo")
-
 const logExactErrorMsg = err => {
   if (typeof err === "object" && err.message) console.log("\x1b[31m%s\x1b[0m", `[ERR] ${err.message}`)
   console.log("\x1b[31m%s\x1b[0m", "[ERR]", err)
