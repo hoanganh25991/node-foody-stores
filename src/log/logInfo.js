e1c2df365c1e79b15a83aac412b0a42ac24cdc84
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-10-09 03:40:51 +0000

    Update logInfo with state, hope it works this time, log(state)(WHAT_TO_LOG)

--- src/log/logInfo.js
+++ src/log/logInfo.js
@@ -1,33 +1,39 @@
 const store = require("../store")
 const logKey = "_log"
-const defaultLogState = {
+const defaultState = {
   logLevel: 1,
   spaceIndent: 2
 }
 
-const getLogState = (addUp = 0, nextState = null) => {
-  const { [logKey]: currState = defaultLogState } = store.getState()
-  const logState = Object.assign(currState, nextState)
-  const { logLevel: currLogLevel } = logState
-  const logLevel = currLogLevel + addUp
-  Object.assign(logState, { logLevel })
-  console.log(JSON.stringify(logState))
-  return logState
+const initLogState = () => {
+  store.setState({ [logKey]: defaultState })
+  return defaultState
 }
 
-const getPadding = (addUp = 0, nextState = null) => {
-  const logState = getLogState(nextState)
-  const { logLevel, spaceIndent } = logState
-
-  const padding = Array(logLevel * spaceIndent + 1).join(" ")
-
-  return padding
+const getState = () => {
+  let { [logKey]: currState } = store.getState()
+  if (!currState) {
+    currState = initLogState()
+  }
+  return currState
 }
 
-const logInfo = (addUp = 0, nextState = null) => (logs, style = "%s") => {
-  const padding = getPadding(addUp, nextState)
-  const { logLevel } = getLogState(addUp, nextState)
-  const paddingWithRootSlash = logLevel > 0 ? `${padding}\\__` : padding
+const getPadding = state => {
+  const { logLevel, spaceIndent } = state
+  return Array(logLevel * spaceIndent + 1).join(" ")
+}
+
+const updateState = (nextState = null) => {
+  const currState = getState()
+  const state = Object.assign({}, currState, nextState)
+  store.setState({ [logKey]: state })
+  return state
+}
+
+const logInfo = (nextState = null) => (logs, style = "%s") => {
+  const state = updateState(nextState)
+  const padding = getPadding(state)
+  const paddingWithRootSlash = state.logLevel > 0 ? `${padding}\\__` : padding
 
   const isArr = Array.isArray(logs)
   const isStr = typeof logs === "string"
@@ -46,15 +52,15 @@ const logInfo = (addUp = 0, nextState = null) => (logs, style = "%s") => {
       break
     }
   }
-
-  return logLevel
 }
 
-store.setState({ [logKey]: defaultLogState })
-
-logInfo.getDefaultState = () => defaultLogState
-logInfo.getLogKey = () => logKey
-logInfo.getLogState = getLogState
-logInfo.getPadding = getPadding
+logInfo.indent = addUp => {
+  const currState = getState()
+  const { logLevel: curr } = currState
+  const logLevel = curr + addUp
+  const state = Object.assign({}, currState, { logLevel })
+  store.setState({ [logKey]: state })
+  return state
+}
 
 module.exports = logInfo
